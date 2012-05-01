af74ac06d2909a05c60eb274d5bba2bd336dde4a
Stephen Nelson-Smith <sns@opscode.com>
2012-04-30 20:27:35 -0700

    Bare bones chef repo

---
+++ cookbooks/kodoyanpe/attributes/default.rb
@@ -0,0 +1,30 @@
+default['kodoyanpe']['pkg_path'] = "http://pkgsrc-cdn-oregon.atalanta-systems.com"
+
+default['kodoyanpe']['pkgsrc_pkgs'] = %w{
+  curl-7.25.0.tgz
+  db4-4.8.30.tgz
+  digest-20111104.tgz
+  expat-2.1.0.tgz
+  gcc34-3.4.6nb7.tgz
+  gettext-lib-0.18.1.1.tgz
+  gmake-3.82nb3.tgz
+  heimdal-1.5.2nb2.tgz
+  libgetopt-1.4.4.tgz
+  libiconv-1.14nb2.tgz
+  libidn-1.24.tgz
+  libyaml-0.1.2.tgz
+  nbpatch-20100124.tgz
+  openssl-0.9.8w.tgz
+  p5-Authen-SASL-2.15nb1.tgz
+  p5-Digest-HMAC-1.03.tgz
+  p5-Digest-MD5-2.51nb1.tgz
+  p5-Digest-SHA-5.71.tgz
+  p5-Error-0.17017.tgz
+  p5-GSSAPI-0.28nb2.tgz
+  p5-MIME-Base64-3.13nb1.tgz
+  perl-5.14.2nb5.tgz
+  readline-6.2.tgz
+  ruby193-base-1.9.3p194.tgz
+  scmgit-base-1.7.10.tgz
+  sqlite3-3.7.11nb1.tgz
+}
---
+++ solo.rb
@@ -0,0 +1,4 @@
+cookbook_path   "cookbooks"
+
+# caching options
+file_cache_path           "/var/cache/chef"
